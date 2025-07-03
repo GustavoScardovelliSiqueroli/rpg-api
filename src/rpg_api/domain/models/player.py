7db75dab2e9b294b4470b3362a9d557ff3b8c4e2
gustavo <gustavosecardovelli@gmail.com>
2025-07-03 17:20:00 -0300

    feat: player and diagram

--- src/rpg_api/domain/models/player.py
+++ src/rpg_api/domain/models/player.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Integer, String
+from decimal import Decimal
+
+from sqlalchemy import Integer, Numeric, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from rpg_api.domain.models.base import Base
@@ -7,3 +9,13 @@ from rpg_api.domain.models.base import Base
 class Player(Base):
     id_player: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+
+    productivity: Mapped[int] = mapped_column(Integer, nullable=False)
+    energy: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    money: Mapped[Decimal] = mapped_column(
+        Numeric(precision=15, scale=2), nullable=False
+    )
+    network: Mapped[Decimal] = mapped_column(
+        Numeric(precision=15, scale=2), nullable=False
+    )
